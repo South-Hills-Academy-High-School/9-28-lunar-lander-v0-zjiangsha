3800ed37d69379e7a9790bbb68d5003dc9f94019
zjiangsha <113388192+zjiangsha@users.noreply.github.com>
2022-09-28 12:30:29 -0700

    working on map and enging

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
+namespace SpriteKind {
+    export const map = SpriteKind.create()
+    export const rocketengine = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.ay = -25
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundCenter, function (sprite, location) {
     mySprite.setPosition(0, -1)
 })
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.ay = 20
+})
 let mySprite: Sprite = null
+let angle = 0
 tiles.setCurrentTilemap(tilemap`level1`)
 effects.clouds.startScreenEffect()
 mySprite = sprites.create(img`
@@ -22,15 +33,20 @@ mySprite = sprites.create(img`
     . . . 2 2 e e 4 4 4 2 e e . . . 
     . . . . . 2 2 e e e e . . . . . 
     `, SpriteKind.Player)
+let mySprite3 = sprites.create(img`
+    3 7 
+    7 3 
+    `, SpriteKind.rocketengine)
 scene.cameraFollowSprite(mySprite)
 scaling.scaleByPercent(mySprite, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
 mySprite.ay = 20
 let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
-let mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.Player)
+let mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
 game.onUpdate(function () {
     mySprite2.destroy()
     myMinimap = minimap.minimap(MinimapScale.Half, 2, 0)
     minimap.includeSprite(myMinimap, mySprite, MinimapSpriteScale.MinimapScale)
-    mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.Player)
+    mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
     mySprite2.setPosition(mySprite.left, mySprite.top)
+    mySprite3.setPosition(mySprite.x, mySprite.y)
 })
