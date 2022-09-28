e4989ed5d4992086d2107477bab2a7c9d7290715
zjiangsha <113388192+zjiangsha@users.noreply.github.com>
2022-09-28 12:14:58 -0700

    working on map

--- main.ts
+++ main.ts
@@ -1,1 +1,36 @@
-
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundCenter, function (sprite, location) {
+    mySprite.setPosition(0, -1)
+})
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+effects.clouds.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+scaling.scaleByPercent(mySprite, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+mySprite.ay = 20
+let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
+let mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.Player)
+game.onUpdate(function () {
+    mySprite2.destroy()
+    myMinimap = minimap.minimap(MinimapScale.Half, 2, 0)
+    minimap.includeSprite(myMinimap, mySprite, MinimapSpriteScale.MinimapScale)
+    mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.Player)
+    mySprite2.setPosition(mySprite.left, mySprite.top)
+})
